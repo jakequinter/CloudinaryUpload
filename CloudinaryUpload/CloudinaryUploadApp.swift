b3386454cd005737ee8854759c327cef9f492bfc
Jake Quinter <hello@jakequinter.io>
2023-04-24 16:32:22 -0400

    init

---
+++ CloudinaryUpload/CloudinaryUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudinaryUploadApp.swift
+//  CloudinaryUpload
+//
+//  Created by Jake Quinter on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CloudinaryUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
